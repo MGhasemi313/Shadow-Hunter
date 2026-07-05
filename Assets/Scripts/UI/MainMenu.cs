bf2b673fb5a4d5c4bef4fd1f574abe9ab984f6eb
MGhasemi313 <mgolzar1382@gmail.com>
2026-07-05 16:01:03 +0330

    add feature to Main Menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void Help()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
